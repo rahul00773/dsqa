f430a954a2f1134b2265c4592ccb95fa3b8aedac
rahul00773 <er.rahul337@gmail.com>
2020-01-29 00:32:02 +0530

    added 3 problem--> MissingNumberUsingXor ThreeSum Find Mediun of two sorted array

---
+++ src/main/java/array/FindMissingNumberInArrayUsingXorOperation.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package array;
+
+/**
+ * @author rahul.kumar
+ * @version $Id: FindMissingNumberInArrayUsingXorOperation.java, v 0.1 2020-01-28 23:51 rahul.kumar Exp $$
+ */
+public class FindMissingNumberInArrayUsingXorOperation {
+}
---
+++ src/main/java/array/LongestSubSequence.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package array;
+
+/**
+ * @author rahul.kumar
+ * @version $Id: LongestSubSequence.java, v 0.1 2020-01-28 01:54 rahul.kumar Exp $$
+ */
+public class LongestSubSequence {
+}
