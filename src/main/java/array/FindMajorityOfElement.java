701c2bfec976af8230b410bf722d0452b591b373
rahul0073 <er.rahul337@gmail.com>
2020-02-07 23:22:31 +0530

    added program to Find Minimum difference

--- src/main/java/array/FindMajorityOfElement.java
+++ src/main/java/array/FindMajorityOfElement.java
@@ -5,32 +5,33 @@ package array;
  * @author rahul.kumar
  * @version $Id: FindMajorityOfElement.java, v 0.1 2020-02-06 01:04 rahul.kumar Exp $$
  */
+
+
+/**
+ * Majority of Element defination -  if element presence in array is greater than n/2. Where n is length of array
+ * This problem can be achieved  by Boyer Mayor Majority algorithm
+ */
 public class FindMajorityOfElement {
 
     public static void main(String[] args){
 
-        int[] arr ={2,2};
+        int[] arr ={2,2,2,0,4,3,2,3,1,2,2,2,4,1,2};
         System.out.println(majorityElement(arr));
 
     }
 
     public static int majorityElement(int[] arr){
         int m = -1;
-
         int i =0;
-
         for(int j =0; j<arr.length; j++){
-
             if(i == 0){
                 m = arr[j];
                 i=1;
             }
-
             else if(m == arr[j]){
 
                 i++;
             }
-
             else {
                 i--;
             }
---
+++ src/main/java/array/FindMinimumDifferenceBetweenTwoElements.java
@@ -0,0 +1,98 @@
+
+package array;
+import java.util.*;
+
+/**
+ * @author rahul.kumar
+ * @version $Id: FindMinimumDifferenceBetweenTwoElements.java, v 0.1 2020-02-07 23:01 rahul.kumar Exp $$
+ */
+public class FindMinimumDifferenceBetweenTwoElements {
+
+
+
+    private static final int MAX = 100001;
+    public static void main(String[] args){
+
+        int[] arr = {1, 2, 3, 4};
+
+        System.out.println(getMinimumDifference(arr));
+
+
+    }
+
+    public static int getMinimumDifference(int[] arr){
+
+        int[] freq = new int[MAX];
+
+        for(int i = 0; i < arr.length-1; i++)
+
+        {
+
+            freq[i] = 0;
+
+        }
+
+        for (int i = 0; i < arr.length-1; i++)
+
+        {
+
+
+
+            // Update the frequency of current element
+
+            freq[arr[i]]++;
+
+
+
+            // If current element appears more than once
+
+            // then the minimum absolute difference
+
+            // will be 0 i.e. |arr[i] - arr[i]|
+
+            if (freq[arr[i]] > 1)
+
+                return 0;
+
+        }
+
+        int mn = Integer.MAX_VALUE;
+
+        for (int i = 0; i < MAX; i++)
+
+        {
+
+            if (freq[i] > 0)
+
+            {
+
+                i++;
+
+                int cnt = 1;
+
+                while ((freq[i] == 0) && (i != MAX - 1))
+
+                {
+
+                    cnt++;
+
+                    i++;
+
+                }
+
+                mn = Math.min(cnt, mn);
+
+                i--;
+
+            }
+
+        }
+
+
+
+        return mn;
+
+
+
+    }
+}
