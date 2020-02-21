838f940c187f6c282bbb85abe852a12885781aee
rahul0073 <er.rahul337@gmail.com>
2020-02-22 00:56:06 +0530

    Longest Consequetive Substring

--- src/main/java/array/LongestSubSequence.java
+++ src/main/java/array/LongestSubSequence.java
@@ -1,8 +1,7 @@
-/**
- * Alipay.com Inc.
- * Copyright (c) 2004-2020 All Rights Reserved.
- */
+
 package array;
+
+import java.util.HashSet;
 
 /**
  * @author rahul.kumar
@@ -11,6 +10,44 @@ package array;
 public class LongestSubSequence {
 
     public static void main(String[] args){
+
+        int[] arr = {2,9,1,3,4,10,20};
+
+        System.out.println(getLongestConsequetiveSequence(arr));
+
+    }
+
+    public static int getLongestConsequetiveSequence(int[] arr){
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int i =0; i<arr.length; i++){
+
+            hashSet.add(arr[i]);
+
+        }
+
+        int count = 0;
+
+        for (int i =0; i<arr.length; i++){
+
+            if(!hashSet.contains(arr[i]-1)){
+
+                int no = arr[i];
+                while (hashSet.contains(no)){
+                    no++;
+
+                }
+
+
+                if(count<no-arr[i]){
+                    count = no-arr[i];
+                }
+            }
+        }
+return count;
+
+
 
     }
 
