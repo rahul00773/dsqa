4c0147386bd65d1010c10253891eeb505a6c8433
rahul0073 <er.rahul337@gmail.com>
2020-02-24 00:03:54 +0530

    corrected formatting

--- src/main/java/javadatastructure/PriorityQue.java
+++ src/main/java/javadatastructure/PriorityQue.java
@@ -8,33 +8,26 @@ import java.util.PriorityQueue;
  * @author rahul.kumar
  * @version $Id: PriorityQue.java, v 0.1 2020-02-23 23:32 rahul.kumar Exp $$
  */
-public class PriorityQue  {
+public class PriorityQue {
 
 
-
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         PriorityQueue<Strudent> priorityQueue = new PriorityQueue();
-
         priorityQueue.add(new Strudent(4, "shyam"));
-      priorityQueue.add(new Strudent(1, "rahul"));
-     priorityQueue.add(new Strudent(2, "data"));
-      priorityQueue.add(new Strudent(3, "data"));
-
+        priorityQueue.add(new Strudent(1, "rahul"));
+        priorityQueue.add(new Strudent(2, "data"));
+        priorityQueue.add(new Strudent(3, "data"));
 
         Iterator<Strudent> srt = priorityQueue.iterator();
 
-        while (srt.hasNext()){
+        while (srt.hasNext()) {
             System.out.println("the item of the queue" + priorityQueue.poll().toString());
         }
 
 
-
     }
-
-
 
 
 }
