e7345f1d84f845c4230b3c939ea5ec9f39e2fef1
rahul0073 <er.rahul337@gmail.com>
2020-02-29 00:40:41 +0530

    merged twosorted List

---
+++ src/main/java/linkedlist/MergeTwoSortedLinkedList.java
@@ -0,0 +1,9 @@
+
+package linkedlist;
+
+/**
+ * @author rahul.kumar
+ * @version $Id: MergeTwoSortedLinkedList.java, v 0.1 2020-02-29 00:40 rahul.kumar Exp $$
+ */
+public class MergeTwoSortedLinkedList {
+}
