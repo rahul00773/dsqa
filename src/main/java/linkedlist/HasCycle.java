f5a0decae4a4e32b055c3bcb02c0976f92d78acf
rahul0073 <er.rahul337@gmail.com>
2020-03-02 01:51:24 +0530

    has cycle in linked list

---
+++ src/main/java/linkedlist/HasCycle.java
@@ -0,0 +1,34 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2020 All Rights Reserved.
+ */
+package linkedlist;
+
+/**
+ * @author rahul.kumar
+ * @version $Id: HasCycle.java, v 0.1 2020-03-02 01:50 rahul.kumar Exp $$
+ */
+public class HasCycle {
+
+
+    public boolean hasCycle(LinkedList.Node head) {
+
+        if(head==null){
+            return false;
+        }
+        LinkedList.Node  slow = head;
+        LinkedList.Node  fast = head.next;
+
+        while(slow!=fast){
+
+            if(fast==null ||fast.next==null){
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+}
