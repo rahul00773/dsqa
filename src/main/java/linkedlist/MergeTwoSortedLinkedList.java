65300ef5fc1a4a5cbc89b95da948e0694812d1c9
rahul0073 <er.rahul337@gmail.com>
2020-03-12 23:12:36 +0530

    final merge two sorted list

--- src/main/java/linkedlist/MergeTwoSortedLinkedList.java
+++ src/main/java/linkedlist/MergeTwoSortedLinkedList.java
@@ -7,16 +7,16 @@ package linkedlist;
  */
 public class MergeTwoSortedLinkedList {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 
         LinkedList linkedList = new LinkedList();
         LinkedList.Node first = new LinkedList.Node(1);
-        LinkedList.Node second = new LinkedList.Node(5);
+        LinkedList.Node second = new LinkedList.Node(2);
         LinkedList.Node third = new LinkedList.Node(3);
-        LinkedList.Node fourth = new LinkedList.Node(7);
+        LinkedList.Node fourth = new LinkedList.Node(4);
         LinkedList.Node fifth = new LinkedList.Node(5);
-        LinkedList.Node sixth = new LinkedList.Node(9);
+        LinkedList.Node sixth = new LinkedList.Node(6);
 
 
         linkedList.head = first;
@@ -43,27 +43,30 @@ public class MergeTwoSortedLinkedList {
         fourth3.next = fifth4;
         fifth4.next = sixth5;
 
-       LinkedList.Node n1=  mergeTwoSortedList(linkedList.head,linkedList1.head);
 
+        System.out.println("First Linked List");
+        linkedList.printLinkedList(linkedList.head);
+        System.out.println("Second Linked List");
+        linkedList.printLinkedList(linkedList1.head);
+
+        LinkedList.Node n1 = mergeTwoSortedList(linkedList.head, linkedList1.head);
+        System.out.println("after sorting  Linked List");
         linkedList.printLinkedList(n1);
 
 
     }
 
-    public static LinkedList.Node mergeTwoSortedList(LinkedList.Node l1, LinkedList.Node l2){
+    public static LinkedList.Node mergeTwoSortedList(LinkedList.Node l1, LinkedList.Node l2) {
 
 
+        LinkedList.Node l3 = new LinkedList.Node(0);
 
 
-        LinkedList.Node l3 =new LinkedList.Node(0);
-
-
-
-        if(l1==null){
+        if (l1 == null) {
             return l2;
         }
 
-        if(l2 == null){
+        if (l2 == null) {
 
             return l1;
         }
@@ -72,7 +75,7 @@ public class MergeTwoSortedLinkedList {
         LinkedList.Node l1current = l1;
         LinkedList.Node l2current = l2;
 
-        LinkedList.Node sor=l3;
+        LinkedList.Node sor = l3;
 /*
         if(l1current.data<=l2current.data){
             sor = l1current;
@@ -85,39 +88,36 @@ public class MergeTwoSortedLinkedList {
         }*/
 
 
-        while(l1current.next!=null && l2current.next!=null){
-
-            if(l1current.data<l2current.data){
-
-                sor.next = l1current;
-             //   sor=l1current;
-                //  l3.val = l1current.val;
-                l1current = l1current.next;
-
+        while (true) {
+            if (l1current == null) {
+                sor.next = l2current;
+                break;
             }
 
-            else{
+            if (l2current == null) {
 
+                sor.next = l1current;
+                break;
+            }
+
+            if (l1current.data <= l2current.data) {
+
+                sor.next = l1current;
+
+                l1current = l1current.next;
+
+            } else {
 
                 sor.next = l2current;
-               // sor=l2current;
                 l2current = l2current.next;
-
 
             }
 
             sor = sor.next;
+
         }
 
-        if(l1current.next==null){
-            sor.next = l2current;
-        }
-
-        else if(l2current.next ==null){
-            sor.next = l1current;
-        }
-
-      return sor.next;
+        return l3.next;
 
     }
 }
