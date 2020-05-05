b4202c28d197456dd4854bfc7a245d4376e1e106
rahul0073 <er.rahul337@gmail.com>
2020-05-05 08:32:56 +0530

    left view of binary treee

--- src/main/java/tree/InorderTraversal.java
+++ src/main/java/tree/InorderTraversal.java
@@ -6,4 +6,40 @@ package tree;
  * @version $Id: InorderTraversal.java, v 0.1 2020-05-03 10:53 rahul.kumar Exp $$
  */
 public class InorderTraversal {
+
+    public static void main(String[] args){
+
+        BinaryTree binaryTree = new BinaryTree();
+
+        binaryTree.root = new TreeNode("a");
+        binaryTree.root.left = new TreeNode("b");
+        binaryTree.root.right = new TreeNode("c");
+        binaryTree.root.left.left = new TreeNode("d");
+        binaryTree.root.left.right = new TreeNode("e");
+        binaryTree.root.left.left.left = new TreeNode("h");
+        binaryTree.root.left.left.right = new TreeNode("i");
+        binaryTree.root.right.left = new TreeNode("f");
+        binaryTree.root.right.right = new TreeNode("g");
+        binaryTree.root.right.left.left = new TreeNode("j");
+        binaryTree.root.right.left.right = new TreeNode("k");
+        binaryTree.root.right.right.right = new TreeNode("l");
+        binaryTree.root.right.right.right.left = new TreeNode("m");
+        binaryTree.root.right.right.right.right = new TreeNode("n");
+
+        inOrderTraversal(binaryTree.root);
+
+    }
+
+    public static void inOrderTraversal(TreeNode treeNode){
+
+        if(treeNode==null){
+            return;
+        }
+
+        System.out.println(treeNode.data);
+
+        inOrderTraversal(treeNode.left);
+        inOrderTraversal(treeNode.right);
+
+    }
 }
--- src/main/java/tree/LeftViewofBinaryTree.java
+++ src/main/java/tree/LeftViewofBinaryTree.java
@@ -6,76 +6,69 @@ import java.util.Queue;
 
 /**
  * @author rahul.kumar
- * @version $Id: DiagonalTraversal.java, v 0.1 2020-05-03 10:54 rahul.kumar Exp $$
+ * @version $Id: LeftViewofBinaryTree.java, v 0.1 2020-05-05 07:23 rahul.kumar Exp $$
  */
-public class DiagonalTraversal {
+public class LeftViewofBinaryTree {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
 
         BinaryTree binaryTree = new BinaryTree();
 
         binaryTree.root = new TreeNode("a");
+
         binaryTree.root.left = new TreeNode("b");
+
         binaryTree.root.right = new TreeNode("c");
+
         binaryTree.root.left.left = new TreeNode("d");
+
         binaryTree.root.left.right = new TreeNode("e");
+
         binaryTree.root.left.left.left = new TreeNode("h");
+
         binaryTree.root.left.left.right = new TreeNode("i");
+
         binaryTree.root.right.left = new TreeNode("f");
+
         binaryTree.root.right.right = new TreeNode("g");
+
         binaryTree.root.right.left.left = new TreeNode("j");
         binaryTree.root.right.left.right = new TreeNode("k");
         binaryTree.root.right.right.right = new TreeNode("l");
         binaryTree.root.right.right.right.left = new TreeNode("m");
         binaryTree.root.right.right.right.right = new TreeNode("n");
 
-
-        printBinaryTreeDiagonally(binaryTree.root);
+        printLevelOrderTraversal(binaryTree.root);
     }
 
-
-    public static void printBinaryTreeDiagonally(TreeNode rootNode) {
-
-
-        TreeNode pointerp = rootNode;
+    public static void printLevelOrderTraversal(TreeNode rootNode){
 
         Queue<TreeNode> queue = new LinkedList<>();
-
         queue.add(rootNode);
-
         queue.add(null);
-
-        while (queue.size() > 0) {
-
-            TreeNode temp = queue.peek();
-
-            queue.remove();
-
+        System.out.println(rootNode.data);
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
             if (temp == null) {
 
-                if (queue.isEmpty()) {
-                    return;
+                if (!queue.isEmpty()) {
+                    System.out.println(((LinkedList<TreeNode>) queue).peekFirst().data);
+                    System.out.println("");
+                    queue.add(null);
                 }
 
-                System.out.println();
-
-                queue.add(null);
-            }
-
-
-            while (temp != null) {
-
-                System.out.print(temp.data + " ");
+            } else {
 
                 if (temp.left != null) {
+                    //System.out.print(temp.data + " ");
                     queue.add(temp.left);
+
                 }
-
-                temp = temp.right;
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
             }
-
         }
-
-
     }
 }
